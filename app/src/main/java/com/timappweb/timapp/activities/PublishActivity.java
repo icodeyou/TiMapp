e297f738d7b86f97c971b957b65fa170fcb23cb0
jeanvallon <jeanvallon44@gmail.com>
2016-01-03 22:36:06 +0100

    Amélioration du design des trois pages pour ajouter un post. + nouveaux logos (Material Design)

--- app/src/main/java/com/timappweb/timapp/activities/PublishActivity.java
+++ app/src/main/java/com/timappweb/timapp/activities/PublishActivity.java
@@ -3,6 +3,9 @@ package com.timappweb.timapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 
 import com.timappweb.timapp.R;
 import com.timappweb.timapp.utils.MyLocationProvider;
@@ -18,7 +21,20 @@ public class PublishActivity extends BaseActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish);
         this.initToolbar(true);
+
+        //Initialize variables
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.checkbox);
+        LinearLayout layout_checkbox = (LinearLayout) findViewById(R.id.layout_checkbox);
+
+        layout_checkbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkBox.toggle();
+            }
+        });
+
     }
+
 
 
     //----------------------------------------------------------------------------------------------
