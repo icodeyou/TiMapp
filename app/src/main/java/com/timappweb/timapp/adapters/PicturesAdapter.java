adb53885580baf7c4183725318e8c9ad8a78e6b0
jeanvallon <jeanvallon44@gmail.com>
2016-04-08 09:44:58 +0200

    bonne taille picture

--- app/src/main/java/com/timappweb/timapp/adapters/PicturesAdapter.java
+++ app/src/main/java/com/timappweb/timapp/adapters/PicturesAdapter.java
@@ -112,8 +112,8 @@ public class PicturesAdapter extends RecyclerView.Adapter<PicturesAdapter.Pictur
             //set height picture to prevent padding on view
             Point size = new Point();
             ((Activity) context).getWindowManager().getDefaultDisplay().getSize(size);
-            int halfScreenWidth = size.x/2;
-            ivPicture.getLayoutParams().height = halfScreenWidth;
+            int screenWidth = size.x;
+            ivPicture.getLayoutParams().height = screenWidth;
         }
 
         @Override
