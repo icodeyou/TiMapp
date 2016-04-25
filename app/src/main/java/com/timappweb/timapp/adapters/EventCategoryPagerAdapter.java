67136bca7102ddd4558ff30092914518191c5af4
jeanvallon <jeanvallon44@gmail.com>
2016-04-25 18:38:38 +0200

    design petit à petit de addEventActivity

--- app/src/main/java/com/timappweb/timapp/adapters/EventCategoryPagerAdapter.java
+++ app/src/main/java/com/timappweb/timapp/adapters/EventCategoryPagerAdapter.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.timappweb.timapp.MyApplication;
 import com.timappweb.timapp.R;
@@ -26,14 +28,25 @@ public class EventCategoryPagerAdapter extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup collection, int position) {
         LayoutInflater inflater = LayoutInflater.from(mContext);
-        ViewGroup layout = null;
+
+        ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.category_image_layout, collection, false);
+        TextView nameCategory = (TextView) layout.findViewById(R.id.category_name);
+        ImageView imageCategory = (ImageView) layout.findViewById(R.id.category_image);
         try {
             EventCategory eventCategory = MyApplication.getCategoryByIndex(position);
-            layout = (ViewGroup) inflater.inflate(eventCategory.getLayoutResId(), collection, false);
+            String capitalizedName =
+                    eventCategory.getName().substring(0, 1).toUpperCase() + eventCategory.getName().substring(1);
+            nameCategory.setText(capitalizedName);
+            imageCategory.setImageResource(eventCategory.getBigImageResId());
         } catch (IndexOutOfBoundsException e) {
-            layout = (ViewGroup) inflater.inflate(R.layout.category_unknown, collection, false);
+            String unknownCategory = mContext.getResources().getString(R.string.unknown_category);
+            String capitalizedUnknownCategory =
+                    unknownCategory.substring(0,1).toUpperCase() + unknownCategory.substring(1);
+            nameCategory.setText(capitalizedUnknownCategory);
+            imageCategory.setImageResource(R.drawable.image_else);
             Log.e(TAG, "Unknown category for pager: " + position);
         }
+
         collection.addView(layout);
         return layout;
     }
