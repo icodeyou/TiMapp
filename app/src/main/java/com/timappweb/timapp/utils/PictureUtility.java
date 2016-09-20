dfda7b7d4a534c3aef9b672e25b6c26776566222
jeanvallon <jeanvallon44@gmail.com>
2016-09-20 18:06:30 +0200

    clean imports

--- app/src/main/java/com/timappweb/timapp/utils/PictureUtility.java
+++ app/src/main/java/com/timappweb/timapp/utils/PictureUtility.java
@@ -3,28 +3,14 @@ package com.timappweb.timapp.utils;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
-import android.widget.ImageView;
 
-import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
-import com.facebook.drawee.view.DraweeView;
-import com.facebook.drawee.view.SimpleDraweeView;
-import com.facebook.imagepipeline.common.ResizeOptions;
-import com.facebook.imagepipeline.request.ImageRequest;
-import com.facebook.imagepipeline.request.ImageRequestBuilder;
-import com.iceteck.silicompressorr.SiliCompressor;
-import com.timappweb.timapp.R;
 import com.timappweb.timapp.config.ConfigurationProvider;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
