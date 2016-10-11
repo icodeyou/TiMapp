41cb47f14bf0a0867474fbadccca99cf9651d161
Stephane <sca.leonard@gmail.com>
2016-10-11 17:50:49 +0200

    Fixing imports

--- app/src/androidTest/java/com/timappweb/timapp/utils/annotations/CreateAuthAction.java
+++ app/src/androidTest/java/com/timappweb/timapp/utils/annotations/CreateAuthAction.java
@@ -1,5 +1,7 @@
 package com.timappweb.timapp.utils.annotations;
 
+import com.google.firebase.auth.FacebookAuthProvider;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
